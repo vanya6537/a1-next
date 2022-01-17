1a9e84e02f0bc8c232eeba6d74f46571483ffd5c
Ivan Katkov <vanya6537@gmail.com>
2022-01-17 09:22:15 +0300

    fix(investments): remove unused imports

--- src/pages-scrollable/services/investments.tsx
+++ src/pages-scrollable/services/investments.tsx
@@ -4,8 +4,6 @@
 
 import type { NextPage } from 'next';
 import React from 'react';
-import { motion } from 'framer-motion';
-import { AnimatedLayout } from 'src/shared/ui/animated-layout';
 
 const ScrollableInvestmentsPage: NextPage = () => {
     return <h1 style={{ padding: '30% 30%' }}>Инвестиции</h1>;
